60c476acc4a3722581fc975a00b89cdbc9cd92a2
Scott Cooper <scttcper@gmail.com>
2018-03-04 19:14:39 -0800

    feat(core): reduce tsconfigs and setup semantic-release

--- build.ts
+++ build.ts
@@ -3,17 +3,16 @@ import { join } from 'path';
 import { copySync } from 'fs-extra';
 import * as camelCase from 'lodash.camelcase';
 import { rollup, InputOptions, OutputOptions } from 'rollup';
-import * as resolve from 'rollup-plugin-node-resolve';
 import * as sourceMaps from 'rollup-plugin-sourcemaps';
 
 const libraryName = 'library';
 
 const inputOptions: InputOptions = {
-  input: `out/public_api.js`,
-  plugins: [resolve(), sourceMaps()],
+  input: `dist/esm/public_api.js`,
+  plugins: [sourceMaps()],
 };
 const outputOptions: OutputOptions = {
-  file: './dist/index.umd.js',
+  file: `./dist/bundles/${libraryName}.umd.js`,
   name: libraryName,
   format: 'umd',
   sourcemap: true,
@@ -26,6 +25,8 @@ async function build() {
   await bundle.write(outputOptions);
 
   // copy files to distribution folder
+  copySync('.git', join(process.cwd(), 'dist/.git'));
+  copySync('package.json', join(process.cwd(), 'dist/package.json'));
   copySync('README.md', join(process.cwd(), 'dist/README.md'));
   copySync('LICENSE', join(process.cwd(), 'dist/LICENSE'));
 }
