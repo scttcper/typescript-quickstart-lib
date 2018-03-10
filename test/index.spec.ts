7f875e59610f41dd2eff7919c8bdfce88e01ffa0
Scott Cooper <scttcper@gmail.com>
2018-03-10 00:13:14 -0800

    chore: remove comment

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,8 +1,5 @@
 import { DummyClass } from '../src';
 
-/**
- * Dummy test
- */
 describe('Dummy test', () => {
   it('should be instantiable', () => {
     expect(new DummyClass()).toBeInstanceOf(DummyClass);
