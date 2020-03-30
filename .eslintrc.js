4a8f76df5365fa080a570eeedf1ea2bbcd824eac
Scott Cooper <scttcper@gmail.com>
2020-03-30 10:39:14 -0700

    chore: upgrade dep

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,5 +10,8 @@ module.exports = {
     '@typescript-eslint/indent': ['error', 2, { SwitchCase: 1 }],
     'comma-dangle': ['error', 'always-multiline'],
     '@typescript-eslint/no-explicit-any': 0,
+    '@typescript-eslint/no-unsafe-member-access': 0,
+    '@typescript-eslint/no-unsafe-call': 0,
+    '@typescript-eslint/no-unsafe-return': 0,
   },
 };
