bcc76d511cad7f212e6915ec5b5a9b350e996452
Scott Cooper <scttcper@gmail.com>
2024-11-17 18:49:19 -0800

    bump dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,15 @@ import config from '@ctrl/eslint-config-biome';
 
 export default [
   {
-    ignores: ['tailwind.config.cjs', 'postcss.config.cjs', 'eslint.config.mjs', 'vite.config.ts'],
+    ignores: [
+      'tailwind.config.cjs',
+      'postcss.config.cjs',
+      'eslint.config.mjs',
+      'vite.config.ts',
+      'dist',
+      'coverage',
+      'build',
+    ],
   },
   ...config,
 ];
